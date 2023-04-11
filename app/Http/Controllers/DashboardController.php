a0969d7cac18ed0643295f166c8565844ba4803a
abdur <abdur.rahimi@kawanlamaretail.com>
2023-04-11 11:51:57 +0700

    asdasd

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,12 +21,17 @@ class DashboardController extends Controller
         $transaksiMonth = Order::select(DB::Raw('tipe, COALESCE(sum(nominal),0) as total'))->whereRaw('MONTH(created_at) = MONTH(now()) and YEAR(created_at) = YEAR(now())');
         $transaksiWeek = Order::select(DB::Raw('tipe, COALESCE(sum(nominal),0) as total'))->whereRaw('YEARWEEK(`created_at`, 1) = YEARWEEK(CURDATE(), 1)');
 
+        $latestOrder = Order::select('order.*','bank.bank as pembayaran')
+        ->leftJoin('bank','bank.id','pembayaran_id');
+
         if(Auth::user()->roles != 'admin'){
             $totalOrder = $totalOrder->where('member_id','=',Auth::user()->id);
 
             $transaksi = $transaksi->where('member_id','=',Auth::user()->id);
             $transaksiMonth = $transaksiMonth->where('member_id','=',Auth::user()->id);
             $transaksiWeek = $transaksiWeek->where('member_id','=',Auth::user()->id);
+
+            $latestOrder = $latestOrder->where('member_id','=',Auth::user()->id);
         }
 
         $totalOrder = $totalOrder->groupBy(DB::Raw('status,tipe'))->get();
@@ -35,13 +40,16 @@ class DashboardController extends Controller
         $transaksiMonth = $transaksiMonth->groupBy(DB::Raw('tipe'))->get();
         $transaksiWeek = $transaksiWeek->groupBy(DB::Raw('tipe'))->get();
 
+        $latestOrder = $latestOrder->orderBy('created_at','desc')->limit(10)->get();
+
         $data = [
             "total" => $totalOrder,
             "transaksi" =>   [
                 "all" => $transaksi,
                 "week" => $transaksiWeek,
                 "month" => $transaksiMonth,
-            ]
+            ],
+            "latest" => $latestOrder,
         ];
 
 
